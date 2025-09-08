07a9c4808b117b4caaa025cbc70c233cba5b3454
JOEL73836 <joelmbera02@email.com>
2025-09-08 13:39:28 +0200

    adding a updated files

--- another/another/settings.py
+++ another/another/settings.py
@@ -12,12 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY') 
+SECRET_KEY = config("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG' , cast=bool) 
+DEBUG = config("DEBUG" , cast=bool) 
 
-ALLOWED_HOSTS = config('DATABASE_URL').split(',') 
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(',') 
 
 
 # Application definition
@@ -72,7 +72,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] =dj_database_url.parse('')
+DATABASES['default']=dj_database_url.parse(config('DATABASE_URL')) 
 
 
 # Password validation
